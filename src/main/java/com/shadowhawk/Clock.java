984b477586d11ffe4f12471138c3893bf88410aa
sonofhawk01 <sonofhawk01@users.noreply.github.com>
2016-05-27 10:21:19 -0400

    initial commit

---
+++ src/main/java/com/shadowhawk/Clock.java
@@ -0,0 +1,61 @@
+package com.shadowhawk;
+
+public class Clock {
+
+	/**
+	 * Size and position for the clock
+	 */
+	protected int xPos, yPos, size;
+	
+	/**
+	 * Whether the clock is currently visible
+	 */
+	protected boolean visible = true;
+	
+	/**
+	 * Get the current size
+	 */
+	public int getSize()
+	{
+		return this.size;
+	}
+	
+	/**
+	 * Get whether the clock is currently visible
+	 */
+	public boolean isVisible()
+	{
+		return this.visible;
+	}
+	
+	/**
+	 * @param xPos
+	 * @param yPos
+	 */
+	public void setPosition(int xPos, int yPos)
+	{
+		this.xPos = Math.max(0,  xPos);
+		this.yPos = Math.max(0,  yPos);
+	}
+	
+	/**
+	 * Set the size of the clock
+	 * 
+	 * @param size new size (min is 32)
+	 */
+	public void setSize(int size)
+	{
+		this.size  = Math.max(32, size);
+	}
+	
+	/**
+	 * Set whether the clock should be visible
+	 * 
+	 * @param visible new visibility setting
+	 */
+	public void setVisible(boolean visible)
+	{
+		this.visible = visible;
+	}
+	
+}
